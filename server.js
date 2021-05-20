5badcff21edc28bd9caf021208fd1a6ff20f41e9
nitin0280 <nitguptaskumar789@gmail.com>
2021-05-20 19:18:05 +0530

    schemas

--- server.js
+++ server.js
@@ -8,6 +8,116 @@ const passportLocalMongoose=require("passport-local-mongoose");
 const {routesHandler } = require( __dirname + "/routes/AllRoutesHandlers.js");
 
 
+const userSchema=new mongoose.Schema({
+	_id : {
+		type : String,
+		required : true
+	},
+	name : {
+		type : String,
+		required : true
+	},
+	email : {
+		type : String,
+		required : true
+	},
+	password : {
+		type : String,
+		required : true
+	},
+	img : {
+		data : Buffer,
+		contentType : String
+	},
+	favicon : {
+		data : Buffer,
+		contentType : String
+	},
+	bio : String,
+	friends : [{
+		_id : String,
+		name : String,
+		favicon : {
+			data : Buffer,
+			contentType : String
+		},
+		messages : [{
+			message: String,
+			timeOfArrival : Date,
+			isMe : Boolean,
+		}]
+	}],
+	following : [{
+		diary_id: String
+	}],
+	myDiaries : [{
+		diary_id: String
+	}],
+	access: [{
+		user_id : String,
+		startTime: Date,
+		endTime: Date,
+		isWrite: Boolean
+	}],
+	notifications: [{
+		notification: String,
+		arrivalTime: Date
+	}],
+});
+const diarySchema=new mongoose.Schema({
+	_id : {
+		type : String,
+		required : true
+	},
+	owner_id : {
+		type : String,
+		required : true
+	},
+	page_ids:[{
+		page_id: String
+	}],
+
+	followers : [{
+		user_id : String,
+		favicon : {
+			data : Buffer,
+			contentType : String
+		}
+	}],
+	numberOfFollowers: Number,
+	access: [{
+		user_id : String,
+		startTime: Date,
+		endTime: Date,
+		isWrite: Boolean
+	}],
+	isPrivate : Boolean
+});
+const pageSchema=new mongoose.Schema({
+	_id : {
+		type : String,
+		required : true
+	},
+	owner_id : {
+		type : String,
+		required : true
+	},
+	diary_id:String,
+	content : String,
+	isPrivate : Boolean,
+	likes : Number,
+	comments : [{
+		comment : String,
+		user_id : String,
+		time : Date,
+		favicon : {
+			data : Buffer,
+			contentType : String
+		},
+	}]
+},{
+	timestamps : true
+});
 // mongoose.connect("mongodb://localhost:27017/", {
 // 							useNewUrlParser: true,
 // 							useUnifiedTopology: true
@@ -21,6 +131,7 @@ const app = express();
 app.use(fileupload());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
 
 
 app.get("/", (req, res) => routesHandler.getDashboard(req,res));
