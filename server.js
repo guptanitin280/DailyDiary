734726a73876ec0b76687e344b32ccca66cba314
nitin0280 <nitguptaskumar789@gmail.com>
2021-05-20 11:55:17 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const app=express();
 const session=require('express-session');
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
+
+const app=express();
+
+app.use(express.static("public"));
