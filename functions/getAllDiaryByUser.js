b61debc94ab50616756d3fed3e5fd55eb39cf396
nitin0280 <nitguptaskumar789@gmail.com>
2021-05-21 13:55:47 +0530

    functons

---
+++ functions/getAllDiaryByUser.js
@@ -0,0 +1,8 @@
+module.exports=async function(username,User) {
+    console.log("from get",username);
+    return await User.findOne({'username': username}, 'myDiaries', function (err, user) {
+        if (err) return console.log(err);
+        console.log("did it run",user);
+       return  user.myDiaries;
+    });
+}
