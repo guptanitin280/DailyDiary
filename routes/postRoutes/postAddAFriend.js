9ca002a00e915b4f90573ec751676620d60498b8
nitin0280 <nitguptaskumar789@gmail.com>
2021-05-22 11:31:39 +0530

    Friends Features

---
+++ routes/postRoutes/postAddAFriend.js
@@ -0,0 +1,26 @@
+module.exports= function(req,res,userModel) {
+    if (req.isAuthenticated()) {
+        const profile = req.body.profile;
+        const user = req.user;
+        console.log(profile);
+        console.log("--------------------");
+        console.log(user);
+        userModel.updateOne({'_id': user._id}, {
+            "$push": {
+                friends: {
+                    '_id': profile._id,
+                    'name': profile.username
+                }
+            }
+        },function(err,response){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(response);
+            }
+        });
+        res.redirect("/dashboard");
+    } else {
+        res.redirect("/login");
+    }
+}
