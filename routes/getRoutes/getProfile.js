6523bcc93932d2394f1f89b50f4ef2591dfcd100
ritikmittal <mittalritik365@gmail.com>
2021-05-23 10:26:05 +0530

    dashboad and profile front-end

--- routes/getRoutes/getProfile.js
+++ routes/getRoutes/getProfile.js
@@ -1,8 +1,14 @@
 module.exports=async function(req,res,userModel) {
     if (req.isAuthenticated()) {
         const profileId = req.params.user_id;
-        const profileUser = await userModel.findById(profileId).exec();
-        res.render("profile", {user: profileUser});
+        let isSelf = false;
+        let profileUser = {};
+        if( req.params.user_id === undefined || req.params.user_id === 's') {
+            isSelf = true;
+        } else {
+            profileUser = await userModel.findById(profileId).exec();
+        }
+        res.render("profile", {user: profileUser, isSelf : isSelf});
     } else {
         res.render("home");
     }
