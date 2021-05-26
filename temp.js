ef719b090ef51003b78865de21d85a527d2bc40a
ritikmittal <mittalritik365@gmail.com>
2021-05-26 09:51:52 +0530

    nota

--- temp.js
+++ temp.js
@@ -1,15 +1,21 @@
-const { loremIpsum } = require("lorem-ipsum");
+const express=require("express");
+const bodyParser=require("body-parser");
+const app = express();
 
-console.log(loremIpsum({
-  count: 7,                // Number of "words", "sentences", or "paragraphs"
-  format: "html",         // "plain" or "html"
-  paragraphLowerBound: 3,  // Min. number of sentences per paragraph.
-  paragraphUpperBound: 7,  // Max. number of sentences per paragarph.
-  random: Math.random,     // A PRNG function
-  sentenceLowerBound: 5,   // Min. number of words per sentence.
-  sentenceUpperBound: 15,  // Max. number of words per sentence.
-  suffix: "\n",            // Line ending, defaults to "\n" or "\r\n" (win32)
-  units: "paragraphs",      // paragraph(s), "sentence(s)", or "word(s)"
-         // Array of words to draw from
-}))
+const fs = require('fs');
 
+app.get("/", (req,res) => {
+  fs.readFile("public/images/pic1.jpeg" , (err, data) => {
+      const str = data.toString('base64')
+      // res.send("data:image/jpeg;base64," + str)
+      res.writeHead(200, {
+     'Content-Type': 'image/png',
+     'Content-Length': str.length
+   });
+      res.end(data)
+})
+})
+
+app.listen(3000 , () => {
+  console.log("listening to port 3000")
+})
